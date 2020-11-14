cebca3e7a952e9e0285e795deda9ce494e12d01a
CarolY <mon_alisa75@yahoo.com>
2020-11-13 20:37:47 -0800

    Set up basic server format.

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+//Dependencies
 const express = require ('express');
+
 
 const app = express();
 
+//Set initial PORT
 const PORT = process.env.PORT || 3000;
+
+//Set up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
