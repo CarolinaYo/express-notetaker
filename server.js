2b2b80ee525760b732255094e3ef368fa5a99064
CarolY <mon_alisa75@yahoo.com>
2020-11-14 21:41:56 -0800

    activate apiRoute.js.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Router
-// require("./routes/apiRoute")(app);
+require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 
 //Listener
