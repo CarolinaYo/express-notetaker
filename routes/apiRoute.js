2a160ef7d618bd268494fe293844e1d7dfe37863
CarolY <mon_alisa75@yahoo.com>
2020-11-14 22:49:40 -0800

    File formatted and removed extra comments.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,89 +1,75 @@
-//dependencies
+//Dependencies
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 // internal file
 const noteData = require("../db/db.json");
 
-module.exports = function(app) {
-   
-    // API GET Requests
-
-    app.get("/api/notes", function(req, res) {
-        fs.readFile("./db/db.json", (err, data) => {
-            if (err) 
-              console.log(err); 
-            else { 
-                //return new note to the client
-                let parseNote = JSON.parse(data);
-                res.send(parseNote); 
-
-            }      
-        });
+module.exports = function (app) {
+  // API GET Requests
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) console.log(err);
+      else {
+        //return new note to the client
+        let parseNote = JSON.parse(data);
+        res.send(parseNote);
+      }
     });
+  });
 
+  // API POST Requests
+  app.post("/api/notes", function (req, res) {
+    //receive new note to save on the request body
+    //apply unique id:
 
-    // API POST Requests ????
-    app.post("/api/notes", function(req, res) {
-        //receive new note to save on the request body 
-        //apply unique id:
-         
-        let newNote = {
-            id: uuidv4(),
-            title: req.body.title,
-            text: req.body.text
-        };
+    let newNote = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
 
-        fs.readFile("./db/db.json", (err, data) => {
-            if (err) 
-              console.log(err); 
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) console.log(err);
 
-            
-            let allNotes = JSON.parse(data);
+      let allNotes = JSON.parse(data);
 
-            allNotes.push(newNote);
+      allNotes.push(newNote);
 
-            fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-                res.send(newNote); 
-                })
-            //return new note to the client
-                   
-
-              
-        })
-      
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        //return new note to the client
+        res.send(newNote);
+      });
     });
- 
-  
-    app.delete("/api/notes/:id", function(req, res) {
-//       // Need to read all notes from db.json and remove note with the given id property and rewrite notes tot eh db.json file
-        fs.readFile("./db/db.json", (err, data) => {
-            let noteIndex;
-            if (err) 
-            console.log(err); 
-        
-            let allNotes = JSON.parse(data);
-            allNotes.filter(function(note, index){
-                if (note.id === req.params.id){
-                    noteIndex = index
-                    return;
-                }
+  });
 
-            })
-            allNotes.splice(noteIndex);
+  app.delete("/api/notes/:id", function (req, res) {
+    // Read all notes from db.json and remove note with the given id property and rewrite notes tot eh db.json file
+    fs.readFile("./db/db.json", (err, data) => {
+      let noteIndex;
+      if (err) console.log(err);
 
-            fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-                res.sendStatus(200); 
-            })
-         });
-    
-    })
-}
+      let allNotes = JSON.parse(data);
+      allNotes.filter(function (note, index) {
+        if (note.id === req.params.id) {
+          noteIndex = index;
+          return;
+        }
+      });
+
+      allNotes.splice(noteIndex);
+
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.sendStatus(200);
+      });
+    });
+  });
+};
